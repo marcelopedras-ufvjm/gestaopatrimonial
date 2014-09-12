95b93762b559981bcd5eededd1341bc76b7a4a88
Marcelo Pedras <marcelo.pedras@ict.ufvjm.edu.br>
2014-09-12 13:01:00 -0300

    Adicionado testes preliminares para resources.

--- test/models/resource_test.rb
+++ test/models/resource_test.rb
@@ -4,4 +4,18 @@ class ResourceTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "responsible for resource must be tutor" do
+    printer = resources(:large_printer_hp)
+    printer.user = users(:user1)
+    assert_equal(false, printer.valid?)
+    printer.user = users(:tutor)
+    assert(printer.valid?)
+  end
+
+  test "is not mandatory that a resource has a tutor" do
+    printer = resources(:large_printer_hp)
+    printer.user = nil
+    assert(printer.valid?)
+  end
 end

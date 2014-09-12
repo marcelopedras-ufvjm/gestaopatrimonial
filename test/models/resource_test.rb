09dcb90fcb95629012514b3f44530450d29f2c00
Marcelo Pedras <marcelo.pedras@ict.ufvjm.edu.br>
2014-09-12 15:21:27 -0300

    Adicionado mais testes para resources.

--- test/models/resource_test.rb
+++ test/models/resource_test.rb
@@ -5,7 +5,7 @@ class ResourceTest < ActiveSupport::TestCase
   #   assert true
   # end
 
-  test "responsible for resource must be tutor" do
+  test 'responsible for resource must be tutor' do
     printer = resources(:large_printer_hp)
     printer.user = users(:user1)
     assert_equal(false, printer.valid?)
@@ -13,9 +13,33 @@ class ResourceTest < ActiveSupport::TestCase
     assert(printer.valid?)
   end
 
-  test "is not mandatory that a resource has a tutor" do
+  test 'is not mandatory that a resource has a tutor' do
     printer = resources(:large_printer_hp)
     printer.user = nil
     assert(printer.valid?)
+  end
+
+  test 'name is mandatory' do
+    pc = resources(:hp3000)
+    pc.name = ""
+    assert_equal(false, pc.valid?)
+  end
+
+  test 'category is mandatory' do
+    pc = resources(:hp3000)
+    pc.category = nil
+    assert_equal(false, pc.valid?)
+  end
+
+  test 'state is mandatory' do
+    pc = resources(:hp3000)
+    pc.state = nil
+    assert_equal(false, pc.valid?)
+  end
+
+  test 'localization is mandatory' do
+    pc = resources(:hp3000)
+    pc.localization = nil
+    assert_equal(false, pc.valid?)
   end
 end
